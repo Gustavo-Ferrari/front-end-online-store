e47c4432947c18f83925a175f2227802f6620b52
Gustavo-Ferrari <gustavod.ferrari@gmail.com>
2022-02-28 18:15:19 -0300

    Updates

---
+++ precache-manifest.beafeee4d6ddc0b505c81ac64e476f36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30a6d995a48ceb270883c3f0c4b6581e",
+    "url": "/front-end-online-store/index.html"
+  },
+  {
+    "revision": "285e66d75ad83c285a98",
+    "url": "/front-end-online-store/static/css/main.63f33e21.chunk.css"
+  },
+  {
+    "revision": "6810b4842663a4d0049b",
+    "url": "/front-end-online-store/static/js/2.e8a91545.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/front-end-online-store/static/js/2.e8a91545.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "285e66d75ad83c285a98",
+    "url": "/front-end-online-store/static/js/main.6c04d9b6.chunk.js"
+  },
+  {
+    "revision": "9fd8b18ceee1540cab08",
+    "url": "/front-end-online-store/static/js/runtime-main.0d3c3230.js"
+  }
+]);
